6e8a844abd2ea5a536480fd69023de47b4133951
Kimhawon <dkckacka12@gmail.com>
2025-06-16 11:04:33 +0900

    Feat 현재씬이 테스트 씬일 경우 Splash로 안가지게 설정

--- IdleGameProject/Assets/02. Script/Editor/EditorStartInit.cs
+++ IdleGameProject/Assets/02. Script/Editor/EditorStartInit.cs
@@ -1,11 +1,19 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 [InitializeOnLoad]
 public class EditorStartInit
 {
     static EditorStartInit()
     {
+        var currentSceneName = SceneManager.GetActiveScene().name;
+        if (currentSceneName == "Test")
+        {
+            EditorSceneManager.playModeStartScene = null;
+            return;
+        }
+        
         var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
         var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
         EditorSceneManager.playModeStartScene = sceneAsset;

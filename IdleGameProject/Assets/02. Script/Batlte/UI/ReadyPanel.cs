21f8463c568acf2a467df5fc502668746446d5a9
Kimhawon <dkckacka12@gmail.com>
2025-06-18 16:51:33 +0900

    Fix ReadyUI가 있을 때 뒤에 버튼이 눌리던 버그 수정

--- IdleGameProject/Assets/02. Script/Batlte/UI/ReadyPanel.cs
+++ IdleGameProject/Assets/02. Script/Batlte/UI/ReadyPanel.cs
@@ -3,12 +3,14 @@ using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace IdleProject.Battle.UI
 {
     public class ReadyPanel : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI readyText;
+        [SerializeField] private Image raycastBlockImage;
 
         [SerializeField] private float introDuration;
         [SerializeField] private float textChangeDuration;
@@ -24,13 +26,21 @@ namespace IdleProject.Battle.UI
         {
             _introSequence = DOTween.Sequence();
 
-            _introSequence.OnStart(() => { transform.localPosition = new Vector3(-1920f, 0f, 0f); });
+            _introSequence.OnStart(() =>
+            {
+                transform.localPosition = new Vector3(-1920f, 0f, 0f);
+                raycastBlockImage.raycastTarget = true;
+            });
             _introSequence.Append((transform as RectTransform).DOLocalMoveX(0f, introDuration));
             _introSequence.AppendInterval(textChangeDuration);
             _introSequence.AppendCallback(() => { readyText.text = "Fight"; });
             _introSequence.AppendInterval(textChangeDuration);
             _introSequence.Append((transform as RectTransform).DOLocalMoveX(1920f, introDuration));
-            _introSequence.OnComplete(() => onUIPlayEnd?.Invoke());
+            _introSequence.OnComplete(() =>
+            {
+                raycastBlockImage.raycastTarget = false;
+                onUIPlayEnd?.Invoke();
+            });
         }
     }
 }

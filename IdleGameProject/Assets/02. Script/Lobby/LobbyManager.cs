ff33ee03b2051f7d723577020ba142eff8774fe4
Kimhawon <dkckacka12@gmail.com>
2025-06-18 16:38:44 +0900

    Feat 로비 버튼 메서드 할당

--- IdleGameProject/Assets/02. Script/Lobby/LobbyManager.cs
+++ IdleGameProject/Assets/02. Script/Lobby/LobbyManager.cs
@@ -1,13 +1,24 @@
 using Cysharp.Threading.Tasks;
 using IdleProject.Core;
+using IdleProject.Core.Loading;
+using IdleProject.Core.UI;
+using IdleProject.Lobby.UI;
 
 namespace IdleProject.Lobby
 {
     public class LobbyManager : SceneController
     {
+        private LobbyUIController _lobbyUIController;
+
+        private const string LOBBY_INIT_TASK = "LobbyInit";
+        
         public override async UniTask Initialize()
         {
-            await UniTask.WaitForSeconds(1f);
+            _lobbyUIController = UIManager.Instance.GetUIController<LobbyUIController>();
+
+            TaskChecker.StartLoading(LOBBY_INIT_TASK, _lobbyUIController.Initialized);
+            
+            await UniTask.WaitUntil(() => TaskChecker.IsTasking(LOBBY_INIT_TASK) is false);
         }
     }
 }
--- IdleGameProject/Assets/02. Script/Lobby/UI/LobbyUIController.cs
+++ IdleGameProject/Assets/02. Script/Lobby/UI/LobbyUIController.cs
@@ -1,8 +1,26 @@
+using Cysharp.Threading.Tasks;
+using IdleProject.Core;
 using IdleProject.Core.UI;
+using UnityEngine;
 
 namespace IdleProject.Lobby.UI
 {
     public class LobbyUIController : UIController
     {
+        public async UniTask Initialized()
+        {
+            UIManager.Instance.GetUI<UIButton>("EquipmentButton").Button.onClick.AddListener(OpenEquipmentPanel);
+            UIManager.Instance.GetUI<UIButton>("DungeonButton").Button.onClick.AddListener(GoToDungeon);
+        }
+
+        private void GoToDungeon()
+        {
+            GameManager.Instance.LoadScene(SceneType.Battle);
+        }
+
+        private void OpenEquipmentPanel()
+        {
+            Debug.Log("OpenEquipmentPanel");
+        }
     }
 }

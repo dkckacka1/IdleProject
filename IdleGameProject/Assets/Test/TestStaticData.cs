559f55cb7d1d240350ddf36360a7102be4eed94c
Kimhawon <dkckacka12@gmail.com>
2025-06-19 14:51:26 +0900

    Revert "Feat 로비에 메인 캐릭터가 보이는 기능 작업"
    This reverts commit 09f30b01476c2e572490a2191d8da79ef12406e7.

--- IdleGameProject/Assets/Test/TestStaticData.cs
+++ IdleGameProject/Assets/Test/TestStaticData.cs
@@ -1,10 +1,9 @@
 using IdleProject.Core.GameData;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "TestStaticData", menuName = "Scriptable Objects/TestStaticData")]
 public class TestStaticData : ScriptableObject
 {
-    public FormationInfoData playerFormationInfo;
-    public FormationInfoData enemyFormationInfo;
+    public SpawnInfoData PlayerSpawnInfo;
+    public SpawnInfoData EnemySpawnInfo;
 }

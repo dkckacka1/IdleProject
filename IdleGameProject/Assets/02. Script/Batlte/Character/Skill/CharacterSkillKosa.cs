265039f3dc266a5aeb0d0bc1f478db8b439a1778
Kimhawon <dkckacka12@gmail.com>
2025-07-01 18:31:23 +0900

    Feat Kosa캐릭터 작업

---
+++ IdleGameProject/Assets/02. Script/Batlte/Character/Skill/CharacterSkillKosa.cs
@@ -0,0 +1,11 @@
+﻿namespace IdleProject.Battle.Character.Skill
+{
+    public class CharacterSkillKosa : CharacterSkill
+    {
+        public override void SetAnimationEvent(AnimationEventHandler eventHandler)
+        {
+            // TODO 
+            
+        }
+    }
+}

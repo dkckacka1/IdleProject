ffbce83b3f33d05d85a73a36451f5026a5e5d3ad
Kimhawon <dkckacka12@gmail.com>
2025-07-13 20:13:47 +0900

    Feat 빌드 시 에디터 코드 작업

--- IdleGameProject/Assets/02. Script/Lobby/UI/SettingPopup/SettingPopup.cs
+++ IdleGameProject/Assets/02. Script/Lobby/UI/SettingPopup/SettingPopup.cs
@@ -3,8 +3,11 @@ using IdleProject.Core.Sound;
 using IdleProject.Core.UI;
 using IdleProject.Lobby.UI;
 using IdleProject.Lobby.UI.SettingPopup;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif 
 
 namespace IdleProject
 {

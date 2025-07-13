0c8cc9888fb3d452d273486b6d509a0e5a0c0e92
Kimhawon <dkckacka12@gmail.com>
2025-07-13 14:58:29 +0900

    Feat 행동 데이터 클래스 분리

---
+++ IdleGameProject/Assets/02. Script/Data/BehaviourData/BehaviourEffectData.cs
@@ -0,0 +1,24 @@
+﻿using IdleProject.Core;
+
+namespace IdleProject.Data.BehaviourData
+{
+    [System.Serializable]
+    public abstract class BehaviourEffectData
+    {
+        public string effectName;
+        public CharacterOffsetType offsetType;
+        public bool canRotate;
+    }
+
+    [System.Serializable]
+    public class LoopEffect : BehaviourEffectData
+    {
+        public float duration;
+    }
+
+    [System.Serializable]
+    public class OneShotEffect : BehaviourEffectData
+    {
+        
+    }
+}
---
+++ IdleGameProject/Assets/02. Script/Data/BehaviourData/BehaviourTargetingData.cs
@@ -0,0 +1,50 @@
+﻿using IdleProject.Core;
+
+namespace IdleProject.Data.BehaviourData
+{
+    [System.Serializable]
+    public abstract class BehaviourTargetingData
+    {
+        public bool isCheckFromTarget;
+    }
+
+    [System.Serializable]
+    public class AITargetingData : BehaviourTargetingData
+    {
+        public CharacterAIType targetAIType;
+    }
+    
+    [System.Serializable]
+    public class RangeTargetingData : BehaviourTargetingData
+    {
+        public float skillRange;
+        public bool distinct;
+        public bool hasSelf;
+        public bool isSelf;
+    }
+
+    [System.Serializable]
+    public class CharacterStateTargetingData : BehaviourTargetingData
+    {
+        public enum CharacterStateTargetType
+        {
+            IsLive,
+        }
+        
+        public bool isNot;
+        public CharacterStateTargetType characterStateTargetType;
+    }
+
+    [System.Serializable]
+    public class SingleTargetingData : BehaviourTargetingData
+    {
+        public enum SingleTargetType
+        {
+            CurrentTarget,
+            NealyController,
+            FarthestController,
+        }
+
+        public SingleTargetType singleTargetType;
+    }
+}
